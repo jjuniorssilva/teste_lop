a85633ea98fdf63c13e4a80be01e188bc1ee7665
Deusimar Junior <junior-plya@live.com>
2020-03-14 09:30:25 -0300

    Add files via upload

---
+++ js.js
@@ -0,0 +1,36 @@
+
+// ------ canvas -------//
+let x = 100,
+  y = 100,
+  angle1 = 0.0,
+  segLength = 50;
+
+function setup() {
+  let canvas = createCanvas(1000, 400);
+  strokeWeight(20.0);
+  stroke(255, 100);
+  canvas.parent('canvas1');
+}
+
+function draw() {
+  background(0);
+
+  dx = mouseX - x;
+  dy = mouseY - y;
+  angle1 = atan2(dy, dx);
+  x = mouseX - cos(angle1) * segLength;
+  y = mouseY - sin(angle1) * segLength;
+
+  segment(x, y, angle1);
+  ellipse(x, y, 20, 20);
+}
+
+function segment(x, y, a) {
+  push();
+  translate(x, y);
+  rotate(a);
+  line(0, 0, segLength, 0);
+  pop();
+}
+
+// ------- fim do segundo canvas -------//
